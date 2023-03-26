cbcb0faede7b684db6acebbbdc53846f597b204c
rainbowhcf <91079861+rainbowhcf@users.noreply.github.com>
2023-03-26 20:38:30 +0800

    Update merge.py

--- merge.py
+++ merge.py
@@ -24,11 +24,11 @@ for waypoint in gpx2.waypoints:
     gpx1.waypoints.append(waypoint)
 
 # 将合并后的gpx数据写入新的gpx文件中
-with open('merge.gpx', 'w', encoding='UTF-8') as merged_file:
+with open(os.path.join(os.environ['HOME'], 'files', 'merge.gpx'), 'w') as merged_file:
     merged_file.write(gpx1.to_xml())
 
-# 存储文件到本地，避免临时文件被清理
-if os.path.exists('merge.gpx'):
-    # 移动文件
-    os.makedirs(os.path.join(os.environ['HOME'], 'files'), exist_ok=True)
-    os.rename('merge.gpx', os.path.join(os.environ['HOME'], 'files', 'merge.gpx'))
+# # 存储文件到本地，避免临时文件被清理
+# if os.path.exists('merge.gpx'):
+#     # 移动文件
+#     os.makedirs(os.path.join(os.environ['HOME'], 'files'), exist_ok=True)
+#     os.rename('merge.gpx', os.path.join(os.environ['HOME'], 'files', 'merge.gpx'))
