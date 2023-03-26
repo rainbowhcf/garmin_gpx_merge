c3536ba51ff49aa4d60674758e37f5c5667aeb9b
rainbowhcf <91079861+rainbowhcf@users.noreply.github.com>
2023-03-26 19:26:03 +0800

    Update merge.py

--- merge.py
+++ merge.py
@@ -1,13 +1,14 @@
 import gpxpy
+import os
 
 dir = r"./files/"
 
 # 读取第一个gpx文件
-with open(dir + '1.gpx', 'r', encoding='UTF-8') as gpx_file1:
+with open(r'files/1.gpx', 'r', encoding='UTF-8') as gpx_file1:
     gpx1 = gpxpy.parse(gpx_file1)
 
 # 读取第二个gpx文件
-with open(dir + '2.gpx', 'r', encoding='UTF-8') as gpx_file2:
+with open(r'files/2.gpx', 'r', encoding='UTF-8') as gpx_file2:
     gpx2 = gpxpy.parse(gpx_file2)
 
 # 将第二个gpx文件中的所有轨迹点添加到第一个gpx文件中
@@ -23,9 +24,8 @@ for waypoint in gpx2.waypoints:
     gpx1.waypoints.append(waypoint)
 
 # 将合并后的gpx数据写入新的gpx文件中
-with open(dir + 'merge.gpx', 'w', encoding='UTF-8') as merged_file:
+with open('merge.gpx', 'w', encoding='UTF-8') as merged_file:
     merged_file.write(gpx1.to_xml())
 
-with open(dir + 'merge.gpx', 'r', encoding='utf-8') as test:
-    for line in test:
-        print(line)
+# 存储文件到本地，避免临时文件被清理
+os.rename('merge.gpx', os.path.join(os.environ['HOME'], 'files', 'merge.gpx'))
