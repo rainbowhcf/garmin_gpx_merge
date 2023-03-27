9884b6b873c97a34c3d613eac24f2406485edcdf
rainbowhcf <91079861+rainbowhcf@users.noreply.github.com>
2023-03-27 09:12:08 +0800

    Update merge.py

--- merge.py
+++ merge.py
@@ -22,25 +22,8 @@ for route in gpx2.routes:
 # 将第二个gpx文件中的所有航点添加到第一个gpx文件中
 for waypoint in gpx2.waypoints:
     gpx1.waypoints.append(waypoint)
-    
-print(os.path.abspath('.'))
-    
+
 # 将合并后的gpx数据写入新的gpx文件中
-with open('/home/runner/work/garmin_gpx_merge/garmin_gpx_merge/files/merge.gpx', 'w', encoding='UTF-8') as merged_file:
-    merged_file.write(gpx1.to_xml())
-    
-# 将文件移动到 GitHub Actions 工作目录之外的目录中
 os.makedirs(os.path.join(os.environ['HOME'], "files"), exist_ok=True)
-os.rename('/home/runner/work/garmin_gpx_merge/garmin_gpx_merge/files/merge.gpx', os.path.join(os.environ['HOME'], "files", "merge.gpx"))
-
-print(os.path.abspath(os.environ['HOME']))
-
-print(os.path.exists('/home/runner/work/garmin_gpx_merge/garmin_gpx_merge/files/merge.gpx'))
-print(os.path.abspath('/home/runner/work/garmin_gpx_merge/garmin_gpx_merge/files/1.gpx'))
-print(os.path.abspath('/home/runner/work/garmin_gpx_merge/garmin_gpx_merge/files/2.gpx'))
-
-# # 存储文件到本地，避免临时文件被清理
-# if os.path.exists('merge.gpx'):
-#     # 移动文件
-#     os.makedirs(os.path.join(os.environ['HOME'], 'files'), exist_ok=True)
-#     os.rename('merge.gpx', os.path.join(os.environ['HOME'], 'files', 'merge.gpx'))
+with open(os.path.join(os.environ['HOME'], "files", "merge.gpx"), 'w', encoding='UTF-8') as merged_file:
+    merged_file.write(gpx1.to_xml())
